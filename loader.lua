a7030f9833635143c488d9acdbcd81ba750e3ac6
evilionx3 <wylan.yandell@gmail.com>
2025-01-12 15:08:58 -0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
--- removed the discord popup since its made by a skid that added a webhook without my knowledge, and he promotes ip grabbing
--- and i dont support ip grabbing skids, fuck that. 
+-- fuck webhooks i hate that shit, its been removed from the script. I wasnt aware of the webhook being in the discord popup i was given.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/evilionx3/kawaii-magnet-part-claim-edition/refs/heads/main/discord-popup-without-webhook-cause-fuck-webhooks-i-hate-that-shit"))() -- discord invite
+
 if game.PlaceId == 6816362093 then
 print("hi - test all gears")
   loadstring(game:HttpGet("https://raw.githubusercontent.com/evilionx3/gear-wall-gui/refs/heads/main/testallgears.lua"))()
